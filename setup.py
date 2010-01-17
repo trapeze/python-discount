a6eb4722172c2295ab7600deb47ac6f66ed565ae
Tamas Kemenczy <tkemenczy@trapeze.com>
2010-01-16 21:19:56 -0500

    Updated INSTALL and setup.py.

--- setup.py
+++ setup.py
@@ -8,49 +8,59 @@ from distutils.command.build_ext import build_ext as _build_ext
 from distutils.core import setup, Extension
 
 
-DISCOUNT_VERSION = '1.5.8'
+DEFAULT_DISCOUNT_VERSION = '1.5.8'
 
-DISCOUNT_DOWNLOAD_URL = (
+
+DEFAULT_DISCOUNT_DOWNLOAD_URL = (
     'http://packages.python.org/discount/'
     'discount-%s.tar.gz'
-) % DISCOUNT_VERSION
+) % DEFAULT_DISCOUNT_VERSION
 
-DISCOUNT_CONFIGURATION = (
+
+DEFAULT_DISCOUNT_CONFIGURE_OPTS = (
     # Use pandoc-style header blocks
-    '--enable-pandoc-header',
+    '--enable-pandoc-header '
 
     # A^B becomes A<sup>B</sup>
-    '--enable-superscript',
+    '--enable-superscript '
 
     # underscores aren'\''t special in the middle of words
-    '--relaxed-emphasis',
+    '--relaxed-emphasis '
 
     # Set tabstops to N characters (default is 4)
-    '--with-tabstops=4',
+    '--with-tabstops=4 '
 
     # Enable >%id% divisions
-    '--enable-div',
+    '--enable-div '
 
     # Enable (a)/(b)/(c) lists
-    '--enable-alpha-list',
+    '--enable-alpha-list '
 
     # Enable memory allocation debugging
-    # '--enable-amalloc',
+    # '--enable-amalloc '
 
     # Turn on all stable optional features
-    # '--enable-all-features',
+    # '--enable-all-features '
 )
 
 
 class build_ext(_build_ext):
     user_options = _build_ext.user_options + [
         ('discount-src-path=', None,
-         'User-defined path to discount source files.'),
+         'Path to discount source files.'),
+
+        ('discount-download-url=', None,
+         'Download url of the discount source files.'),
+
+        ('discount-configure-opts=', None,
+         'Default options passed to ./configure.sh'),
     ]
 
     def initialize_options(self):
         _build_ext.initialize_options(self)
         self.discount_src_path = None
+        self.discount_download_url = DEFAULT_DISCOUNT_DOWNLOAD_URL
+        self.discount_configure_opts = DEFAULT_DISCOUNT_CONFIGURE_OPTS
 
     def build_extension(self, ext):
         if ext.name == '_discount':
@@ -62,15 +72,15 @@ class build_ext(_build_ext):
         if self.discount_src_path is None:
             filepath = os.path.join(
                 self.build_temp,
-                posixpath.basename(DISCOUNT_DOWNLOAD_URL)
+                posixpath.basename(self.discount_download_url)
             )
             if not os.path.lexists(self.build_temp):
                 os.makedirs(self.build_temp)
 
             if not os.path.exists(filepath):
-                print 'Downloading %s...' % DISCOUNT_DOWNLOAD_URL
+                print 'Downloading %s...' % self.discount_download_url
 
-                data = urllib2.urlopen(DISCOUNT_DOWNLOAD_URL)
+                data = urllib2.urlopen(self.discount_download_url)
                 fp = open(filepath, 'wb')
                 fp.write(data.read())
                 fp.close()
@@ -91,7 +101,7 @@ class build_ext(_build_ext):
             current_dir = os.getcwd()
             os.chdir(discount_src_path)
             subprocess.call(
-                ('./configure.sh',) + DISCOUNT_CONFIGURATION,
+                ['./configure.sh',] + self.discount_configure_opts.split(),
                 env=os.environ
             )
             os.chdir(current_dir)
@@ -99,8 +109,12 @@ class build_ext(_build_ext):
         ext.sources = [
             os.path.join(discount_src_path, s) for s in ext.sources
         ]
+
         ext.extra_compile_args += [
             '-I%s' % discount_src_path,
+            '-DVERSION="%s"' % open(
+                os.path.join(discount_src_path, 'VERSION')
+            ).read().strip()
         ]
 
         _build_ext.build_extension(self, ext)
@@ -142,24 +156,10 @@ setup(
     ext_modules=[
         Extension(
             '_discount',
-            extra_compile_args=[
-                '-DVERSION="%s"' % DISCOUNT_VERSION,
-            ],
             sources=[
-                'Csio.c',
-                'amalloc.c',
-                'css.c',
-                'docheader.c',
-                'dumptree.c',
-                'generate.c',
-                'markdown.c',
-                'mkdio.c',
-                'resource.c',
-                'theme.c',
-                'toc.c',
-                'version.c',
-                'xml.c',
-                'xmlpage.c',
+                'amalloc.c', 'Csio.c', 'css.c', 'docheader.c',
+                'dumptree.c', 'generate.c', 'markdown.c', 'mkdio.c',
+                'resource.c', 'toc.c', 'version.c', 'xml.c', 'xmlpage.c',
             ],
         )
     ],
